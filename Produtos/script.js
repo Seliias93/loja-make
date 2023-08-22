c72eacfc25dee0185a2b717d73d56e3bde22f843
Elias De Souza <106115252+Seliias93@users.noreply.github.com>
2023-08-21 22:09:52 -0300

    .

--- Produtos/script.js
+++ Produtos/script.js
@@ -2,3 +2,12 @@ function changeImage(newImage) {
     const mainImage = document.querySelector('.main-image');
     mainImage.src = newImage;
 }
+
+function toggleContent() {
+    var content = document.getElementById("hidden-content");
+    if (content.style.display === "none") {
+      content.style.display = "block";
+    } else {
+      content.style.display = "none";
+    }
+  }
